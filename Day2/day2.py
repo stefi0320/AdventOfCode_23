02dac4a3bf34530826fc41ab563209789d4cfba8
stefi <stefi0320@gmail.com>
2023-12-02 10:55:00 +0100

    fixed

--- Day2/day2.py
+++ Day2/day2.py
@@ -13,8 +13,7 @@ def open_file_safely(file_name):
         return content
 
     except FileNotFoundError:
-        print(f"The file '{
-              file_name}' was not found in the same directory as the script.")
+        print("The file '{file_name}' was not found in the same directory as the script.")
         return None
 
 
