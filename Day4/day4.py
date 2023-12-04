ef6ac7b60fed7d0c71546f52b581b5c2f2b3575f
stefi <stefi0320@gmail.com>
2023-12-04 17:26:53 +0100

    day4

--- Day4/day4.py
+++ Day4/day4.py
@@ -1,6 +1,7 @@
 """Module providing a function reading files."""
 from pathlib import Path
 import time
+import re
 
 def open_file_safely(file_name):
     """ File open """
@@ -16,23 +17,63 @@ def open_file_safely(file_name):
         print("The file '{file_name}' was not found in the same directory as the script.")
         return None
 
+def get_nums(temp):
+    """Spiltter for numbers """
+    return temp.split(' ')
+
+def check_winner(numbers, winner_numbers):
+    """Checks for winning number """
+    power = 0
+    for number in numbers:
+        if number in winner_numbers and number != '':
+            power += 1
+    return power
+
+def check_copies(input_txt, index):
+    """Recursive function for card checking """
+    point = 0
+    copy = 1
+    tmp_input = re.split(': | \| ', input_txt[index].strip())
+    numbers = get_nums(tmp_input[1])
+    winning_numbers = get_nums(tmp_input[2])
+    point = check_winner(numbers, winning_numbers)
+    for i in range(point):
+        copy += check_copies(input_txt, i+1+index)
+    return copy
+        
+    
 def day4():
-   """Day 4 of Advent o f code """
+    """Day 4 of Advent o f code """
 
-   # record start time
-   start = time.time()
+    # record start time
+    start = time.time()
 
-   input_txt = open_file_safely("day2.txt")
+    input_txt = open_file_safely("day4.txt")
 
-
-   for line in input_txt:
-      line = line.strip()
-
-   #part1
-
-
+    #part1
+    card_sum = 0
+    copies = 0
+    
+    for line in enumerate(input_txt):
+        power = -1
+        point = 0
+        tmp_line = re.split(': | \| ', line[1].strip())
+        numbers = get_nums(tmp_line[1])
+        winning_numbers = get_nums(tmp_line[2])
+        power = check_winner(numbers, winning_numbers)
+        if power != 0:
+            point = 2**(power - 1)
+        card_sum += point
+        
+        #part2
+        for i in range(power):
+            copies += check_copies(input_txt, i+1+int(line[0]))
+        copies += 1
+    print('point sum', card_sum)
+    print('copies', copies)
+        
    # record end time
-   end = time.time()
-   print("Runtime :", (end-start) * 10**3, "ms")
+    end = time.time()
+    print("Runtime :", (end-start) * 10**3, "ms")
 
 day4()
