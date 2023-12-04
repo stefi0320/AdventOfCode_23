d732d2f19313b555a5d2dcd04cc678435d9955f8
stefi <stefi0320@gmail.com>
2023-12-04 19:33:53 +0100

    formatting

--- Day3/day3.py
+++ Day3/day3.py
@@ -18,7 +18,9 @@ special_chars = {
     '!'
 }
 
-indexer =[[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, +1], [1, -1], [1, 0], [1, 1]]
+indexer = [[-1, -1], [-1, 0], [-1, 1],
+           [0, -1], [0, +1], [1, -1], [1, 0], [1, 1]]
+
 
 def open_file_safely(file_name):
     """ File open """
@@ -27,12 +29,14 @@ def open_file_safely(file_name):
         file_path = script_dir / file_name
 
         with open(file_path, 'r', encoding="utf-8") as file:
-            content =  list(line for line in (l.strip() for l in file) if line)
+            content = list(line for line in (l.strip() for l in file) if line)
         return content
 
     except FileNotFoundError:
-        print("The file '{file_name}' was not found in the same directory as the script.")
+        print(
+            "The file '{file_name}' was not found in the same directory as the script.")
         return None
+
 
 def find_neighbours(tmp_array, row, col, symbol, part2):
     """ Find neighbours of special char """
@@ -40,19 +44,19 @@ def find_neighbours(tmp_array, row, col, symbol, part2):
     nums = []
     prev_row = 0
     for idx in indexer:
-        if ((row + idx[0] < tmp_array.shape[0]-1)  or (row + idx[0] > 0)) and ((col + idx[1] > 0)  or (col + idx[1] <  tmp_array.shape[1]-1)):
-            if tmp_array[row + idx[0],col + idx[1]] != '.' and  tmp_array[row + idx[0],col + idx[1]] != symbol:
-                #number found
+        if ((row + idx[0] < tmp_array.shape[0]-1) or (row + idx[0] > 0)) and ((col + idx[1] > 0) or (col + idx[1] < tmp_array.shape[1]-1)):
+            if tmp_array[row + idx[0], col + idx[1]] != '.' and tmp_array[row + idx[0], col + idx[1]] != symbol:
+                # number found
                 tmp_row = ''.join(str(x) for x in tmp_array[row + idx[0]])
-                tmp = re.finditer("\d+", tmp_row)
+                tmp = re.finditer(r"\d+", tmp_row)
                 for t in tmp:
-                    if t.start() <= col + idx[1] and col + idx[1]<= t.end():
+                    if t.start() <= col + idx[1] <= t.end():
                         if row + idx[0] == prev_row:
                             if t.group() not in nums:
                                 nums.append(t.group())
                         else:
                             nums.append(t.group())
-                prev_row = row + idx[0]              
+                prev_row = row + idx[0]
     if part2 and len(nums) == 2:
         sum_part = int(nums[0]) * int(nums[1])
     if not part2:
@@ -65,32 +69,33 @@ def day3():
     """Day 3 of Advent o f code """
     # record start time
     start = time.time()
-       
+
     input_txt = open_file_safely("day3.txt")
     input_array = np.array([list(l) for l in input_txt])
-    #print(input_array)
 
     # part1
     sum_part1 = 0
     for s in special_chars:
-        specials = np.char.find(input_array,s)
-        for row in  enumerate(specials):
-            for column in enumerate(row[1]):   
+        specials = np.char.find(input_array, s)
+        for row in enumerate(specials):
+            for column in enumerate(row[1]):
                 if specials[row[0]][column[0]] == 0:
-                    sum_part1 += find_neighbours(input_array, row[0], column[0], s, False)
+                    sum_part1 += find_neighbours(input_array,
+                                                 row[0], column[0], s, False)
     print(sum_part1)
-                  
+
     # part2
-    sum_part2= 0
-    specials = np.char.find(input_array,'*')
-    for row in  enumerate(specials):
-        for column in enumerate(row[1]):   
+    sum_part2 = 0
+    specials = np.char.find(input_array, '*')
+    for row in enumerate(specials):
+        for column in enumerate(row[1]):
             if specials[row[0]][column[0]] == 0:
-                sum_part2 += find_neighbours(input_array, row[0], column[0], s, True)
+                sum_part2 += find_neighbours(input_array,
+                                             row[0], column[0], s, True)
     print(sum_part2)
+    
     # record end time
     end = time.time()
     print("Runtime :", (end-start) * 10**3, "ms")
-
 
 day3()
