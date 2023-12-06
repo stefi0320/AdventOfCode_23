03e079532e4b38ea8448ffbf5af0a3bc0f7eed2a
stefi <stefi0320@gmail.com>
2023-12-06 23:05:39 +0100

    finished

--- Day5/day5.py
+++ Day5/day5.py
@@ -18,6 +18,62 @@ def open_file_safely(file_name):
         print("The file '{file_name}' was not found in the same directory as the script.")
         return None
 
+def check_seed(m, seed):
+    tmp_seeds = []
+    if seed:
+        found_seed = False
+        for rows in m:
+            if rows.s_start <= seed[0] <= seed[1] <= rows.s_end:
+                found_seed = True
+                new_seed = [seed[0] + rows.convert, seed[1] + rows.convert]
+                tmp_seeds.append(new_seed)
+            elif seed[0] < rows.s_start <=  rows.s_end < seed[1]:
+                found_seed = True
+                new_seed = [seed[0], rows.s_start - 1]
+                tempi = check_seed(m,new_seed)
+                if tempi: 
+                    for t in tempi:
+                        if t not in tmp_seeds:
+                            tmp_seeds.append(t)
+                else:
+                    tmp_seeds.append(new_seed)
+                new_seed = [rows.s_start + rows.convert, rows.s_end + rows.convert]
+                tmp_seeds.append(new_seed)
+                new_seed = [rows.s_end + 1, seed[1]]
+                tempi = check_seed(m,new_seed)
+                if tempi: 
+                    for t in tempi:
+                        if t not in tmp_seeds:
+                            tmp_seeds.append(t)
+                else:
+                    tmp_seeds.append(new_seed)
+            elif seed[0] < rows.s_start <= seed[1] <= rows.s_end:
+                found_seed = True
+                new_seed = [seed[0], rows.s_start - 1]
+                tempi = check_seed(m,new_seed)
+                if tempi: 
+                    for t in tempi:
+                          if t not in tmp_seeds:
+                            tmp_seeds.append(t)
+                else:
+                    tmp_seeds.append(new_seed)
+                new_seed = [rows.s_start + rows.convert, seed[1]  + rows.convert]
+                tmp_seeds.append(new_seed)
+            elif rows.s_start <= seed[0] <= rows.s_end < seed[1]:
+                found_seed = True
+                new_seed = [seed[0] + rows.convert, rows.s_end + rows.convert]
+                tmp_seeds.append(new_seed)
+                new_seed = [rows.s_end + 1, seed[1]]
+                tempi = check_seed(m,new_seed)
+                if tempi: 
+                    for t in tempi:
+                        if t not in tmp_seeds:
+                            tmp_seeds.append(t)
+                else:
+                    tmp_seeds.append(new_seed)
+        if not found_seed and seed not in tmp_seeds:
+            tmp_seeds.append(seed)
+    return tmp_seeds
 
 def day5():
     """Day 5 of Advent o f code """
@@ -40,28 +96,42 @@ def day5():
                 tmp_map_to_map = map_to_map(s_start= int(tmp[1]), s_end= (int(tmp[1])+int(tmp[2])-1), convert= (int(tmp[0])-int(tmp[1])))
                 tmp_map.append(tmp_map_to_map)
     maps.append(tmp_map)
-    
+
+    seeds_list = [int(x) for x in input_txt[0].split('seeds: ')[1].split(' ')]
+    seeds_range = []
+    for i, seed in enumerate(seeds_list):
+        if i%2 != 0:
+            continue
+        seeds_range.append([seed, seed + seeds_list[i+1]-1])
+
     #part1
-    seeds = [int(x) for x in input_txt[0].split('seeds: ')[1].split(' ')]
     to_convert = namedtuple('to_convert', 'found_seeds, convert')
     for m in maps:
         convert_list = []
         for rows in m:  
-            found_seeds = list(locate(seeds, lambda x: rows.s_start <= x <= rows.s_end))
+            found_seeds = list(locate(seeds_list, lambda x: rows.s_start <= x <= rows.s_end))
             convert_list.append(to_convert(found_seeds= found_seeds, convert= rows.convert))
         for item in convert_list:
             for seed in item.found_seeds:
-                seeds[seed] = seeds[seed] + item.convert
-    print(min(seeds))
-
-    #record end time
-    end = time.time()
-    print("Runtime :", (end-start) * 10**3, "ms")
+                seeds_list[seed] = seeds_list[seed] + item.convert
+    print(min(seeds_list))
 
     #part2
-    
+    lista = []
+    lista.append(seeds_range)
+    for i in range(7):
+        temp_seeds = []
+        for seed in lista[i]:
+            tmp = check_seed(maps[i], seed)
+            for t in tmp:
+                if t not in temp_seeds:
+                    temp_seeds.append(t)               
+        lista.append(temp_seeds)   
 
+    print(min(lista[-1])[0])
+    
     #record end time
     end = time.time()
     print("Runtime :", (end-start) * 10**3, "ms")
+    
 day5()
