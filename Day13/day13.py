5693c7d9f01e2a7c0ccf24d764de0fda5dac7d31
stefi <stefi0320@gmail.com>
2023-12-15 07:56:12 +0100

    day13 part 2

--- Day13/day13.py
+++ Day13/day13.py
@@ -41,27 +41,41 @@ def find_copy(pattern):
                 return 1                
     return 0
 
-def find_copytwo(pattern):
+def find_copytwo(pattern, found):
     for i in range(len(pattern)-1):
         if pattern[i] == pattern[i+1]:
             j = 1
             while i-j >= 0  and i+1+j < len(pattern):
                 count = 0 
-                for l in range(len(pattern[i])):
-                    if pattern[i-j][l] == pattern[i+1+j][l]:
-                        count+= 1
-                if  len(pattern[i]) - count == 1:
-                    print(pattern[i-j])
-                    print(pattern[i+1+j])   
+                if pattern[i-j] != pattern[i+1+j]:
+                    for l in range(len(pattern[i])):
+                        if pattern[i-j][l] == pattern[i+1+j][l]:
+                            count+= 1
+                    if  len(pattern[i]) - count == 1:
+                        if (i-j == 0 or i+1+j ==  len(pattern)-1)  and i+1 != found: 
+                            return i+1
+                else:
+                    if (i-j == 0 or i+1+j ==  len(pattern)-1) and i+1 != found:
+                        return i+1
                 j+=1
         else:
-            count = 0 
+            count = 0
+            fix = 0
             for l in range(len(pattern[i])):
                 if pattern[i][l] == pattern[i+1][l]:
                     count+= 1
-            if  len(pattern[i]) - count == 1:
-                print(pattern[i])
-                print(pattern[i+1])   
+                    fix = l
+            if  len(pattern[i]) - count == 1 and i+1 != found:
+                if (i == 0 or i+1 ==  len(pattern)-1) and i+1 != found:
+                    return i+1
+                else:
+                    temp_pattern = pattern
+                    temp_pattern[i][fix] = pattern[i+1][fix]
+                    j = 1
+                    while i-j >= 0  and i+1+j < len(temp_pattern) and temp_pattern[i-j] == temp_pattern[i+1+j]:
+                        if i-j == 0 or i+1+j ==  len(temp_pattern)-1:
+                            return i+1
+                        j+=1
     return 0
 
     
@@ -73,6 +87,8 @@ def day13():
     input_txt.append('')
     input_array = []
     patterns = []
+    foundrows = []
+    foundcols = []
     for line in input_txt:
         if line: 
             input_array.append(list(line))
@@ -84,27 +100,33 @@ def day13():
         transposed_patterns.append(transpose_pattern(pattern))
         
     sum_mirrored = 0
-    found_lines = []
-    
+   
     for i, pattern in enumerate(patterns):
         tmp = find_copy(pattern)
         if tmp == 0:
             temp = find_copy(transposed_patterns[i])
-            sum_mirrored+= temp 
-            found_lines.append([i, temp-1, 'col'])
+            sum_mirrored+= temp
+            foundcols.append(temp)
+            foundrows.append(0)
         else:
             sum_mirrored+= 100 * tmp
-            found_lines.append([i, tmp-1, 'row'])
+            foundrows.append(tmp)
+            foundcols.append(0)
 
     print('Part 1:', sum_mirrored)
-    
+    sum_smudge = 0
     for i, pattern in enumerate(patterns):
-        print('rows')
-        find_copytwo(pattern)
-        print('cols')
-        find_copytwo(transposed_patterns[i])
+        found = foundcols[i] if foundcols[i]!= 0 else foundrows[i]
+        tmp = find_copytwo(pattern, foundrows[i])
+        if tmp == 0:
+            temp = find_copytwo(transposed_patterns[i], foundcols[i])
+            print('column ', temp, 'original', found)
+            sum_smudge += temp
+        else:
+            print('row ', tmp, 'original', found)
+            sum_smudge += 100 * tmp
     
-    print('Part 2:')
+    print('Part 2:', sum_smudge)
     
     #record end time
     end = time.time()
